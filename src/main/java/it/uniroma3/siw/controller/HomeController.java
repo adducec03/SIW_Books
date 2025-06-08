c27aea3cc5173daab44fe0e961fadc56e87b41b7
Carmine Adduce <adducec@gmail.com>
2025-06-08 05:24:35 +0200

    aggiunto il fatto che quando fai login il sistema mostra la frase di benvenuto con il nome dell'utente

--- src/main/java/it/uniroma3/siw/controller/HomeController.java
+++ src/main/java/it/uniroma3/siw/controller/HomeController.java
@@ -1,13 +1,27 @@
 package it.uniroma3.siw.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import it.uniroma3.siw.model.Credentials;
+import it.uniroma3.siw.service.CredentialsService;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private CredentialsService credentialsService;
+
     @GetMapping("/")
-    public String index() {
-        return "index"; // carica index.html
+    public String index(Model model, Principal principal) {
+        if (principal != null) {
+            Credentials credentials = credentialsService.getCredentials(principal.getName());
+            model.addAttribute("nomeUtente", credentials.getUtente().getNome());
+        }
+        return "index";
     }
 }
