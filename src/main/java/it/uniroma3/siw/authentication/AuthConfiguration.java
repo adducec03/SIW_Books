87d109f4a1f474f6140b1f080264b56f0b507327
Carmine Adduce <adducec@gmail.com>
2025-06-12 14:04:04 +0200

    finito di modificare il layout della homepage, da aggiustare il layout responsive

--- src/main/java/it/uniroma3/siw/authentication/AuthConfiguration.java
+++ src/main/java/it/uniroma3/siw/authentication/AuthConfiguration.java
@@ -44,7 +44,8 @@ public class AuthConfiguration {
                         .requestMatchers(HttpMethod.GET,
                                 "/", "/index", "/register", "/login", "/autori", "/autore", "/autore/**", "/libri",
                                 "/libro", "/libro/**", "/oauth2/**",
-                                "/**.css", "/images/**", "/js/**", "/favicon.ico", "/logo.png", "/wallpaper.png")
+                                "/**.css", "/images/**", "/js/**", "/favicon.ico", "/logo.png", "/wallpaper.png", "/libro.png", "autori.png", "/recensioni.png",
+                                "/github_icon_light.png", "/instagram_icon_light.png", "/linkedin_icon_light.png")
                         .permitAll()
                         .requestMatchers(HttpMethod.POST, "/register", "/login").permitAll()
                         .requestMatchers("/admin/**").hasAuthority("ADMIN")
